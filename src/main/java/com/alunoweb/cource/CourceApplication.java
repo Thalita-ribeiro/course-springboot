69b0e1480536f7b706a099f422e3fc419f090b68
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2022-01-07 20:53:09 -0300

    Project created

---
+++ src/main/java/com/alunoweb/cource/CourceApplication.java
@@ -0,0 +1,12 @@
+package com.alunoweb.cource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourceApplication.class, args);
+	}
+}
